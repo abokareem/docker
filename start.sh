a771c0fbba88f790b4fe1f3557265002a1ab1b76
abokareem <elnwajha.pal@gmail.com>
2019-11-18 16:21:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ curl -fsSL https://get.docker.com | sh
 DB_HOST="mysql"
 DB_USER="root"
 DB_PASS="admin"
-AB_PORT="8998"
+AB_PORT="80"
 AB2_PORT="8999"
 AB_LOGIN="admin"
-AB_PASS="admin"
+AB_PASS="palman00$$"
 
 if ! [ -d ./www ]; then
 mkdir ./www
@@ -27,13 +27,3 @@ docker-compose exec abantecart-dev chown -R www-data:www-data /var/www/abantecar
 docker-compose exec abantecart-dev /usr/local/bin/php /var/www/abantecart/public_html/install/cli_install.php install --db_host=$DB_HOST --db_user=$DB_USER --db_password=$DB_PASS --db_name=abantecart  --db_driver=amysqli  --db_prefix=abc_ --admin_path=admin  --username=$AB_LOGIN  --password=$AB_PASS  --email=admin@admin.com  --http_server=http://localhost:$AB_PORT/ --with-sample-data
 fi
 
-
-if ! [ -d ./www/abantecart2 ]; then
-sleep 60;
-docker-compose exec abantecart-dev mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -e "create database abantecart2;";
-
-git clone https://github.com/abantecart/abantecart_2.0.git ./www/abantecart2
-docker-compose exec abantecart-dev bash -c "cd /var/www/abantecart2/abantecart/abc/ && composer install"
-docker-compose exec abantecart-dev chown -R www-data:www-data /var/www/abantecart2
-docker-compose exec abantecart-dev bash -c "cd /var/www/abantecart2/abantecart/abc/ && /usr/local/bin/php abcexec install:app  --db_host=$DB_HOST --db_user=$DB_USER --db_password=$DB_PASS --db_name=abantecart2  --db_driver=mysql --admin_secret=admin --admin_path=admin --username=$AB_LOGIN  --password=$AB_PASS  --email=admin@admin.com  --http_server=http://localhost:$AB2_PORT/ --with-sample-data"
-fi
